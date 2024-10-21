1cc398a73854f82154dd5b371fec8d936cec7146
thomas-abecassis <47184168+thomas-abecassis@users.noreply.github.com>
2024-10-21 15:27:53 +0200

    Add files via upload

---
+++ jobFilterVIE.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         Job Filter businessfrance
+// @namespace    http://tampermonkey.net/
+// @version      2024-10-21
+// @description  Remove luxembourg and bruxelles and inactive job offer from businessfrance
+// @author       Thomas Abecassis
+// @match        https://mon-vie-via.businessfrance.fr/offres/recherche?*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=businessfrance.fr
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const removedJobOfferArray = [];
+
+    function removeJobOfferDiv(div){
+        div.style.display = 'none';
+        removedJobOfferArray.push(div);
+    }
+
+    function resetHiddenJobOffers(){
+        removedJobOfferArray.forEach(div=>div.style.display = 'block')
+        removedJobOfferArray.length = 0;
+    }
+
+    function getKeywords() {
+        return GM_getValue('jobFilterKeywords', ['bruxelles', 'luxembourg', '(inactif)']);
+    }
+
+    function saveKeywords(keywords) {
+        GM_setValue('jobFilterKeywords', keywords);
+    }
+
+    function promptForKeywords() {
+        const currentKeywords = getKeywords();
+        const input = prompt('Enter the keywords to be filtered, separated by commas:', currentKeywords.join(', '));
+        if (input !== null) {
+            const newKeywords = input.split(',').map(keyword => keyword.trim()).filter(keyword => keyword !== '');
+            saveKeywords(newKeywords);
+            resetHiddenJobOffers();
+            removeOfferByKeywords(getKeywords());
+        }
+    }
+
+    GM_registerMenuCommand('Edit filter keywords', promptForKeywords);
+
+    function removeOfferByKeywords(keywords) {
+        const jobOfferDivs = document.querySelectorAll('.latest_offers div');
+
+        jobOfferDivs.forEach(div => {
+            const locationElement = div.querySelector('.location');
+
+            if (locationElement) {
+                const location = locationElement.textContent;
+                if (keywords.some(keyword => location.toLowerCase().includes(keyword.toLowerCase())))
+                    removeJobOfferDiv(div);
+            }
+        });
+    }
+
+    function observeJobOffersChanges() {
+        const observer = new MutationObserver(() => {
+            removeOfferByKeywords(getKeywords());
+        });
+
+        const JobOffersContainer = document.querySelector('.home_offre');
+
+        if(JobOffersContainer){
+            observer.observe(JobOffersContainer, {
+                childList: true,
+                subtree: true
+            });
+        }else {
+            console.error('The job offers container hasn\'t been found.');
+        }
+    }
+
+    document.readyState === 'complete' ? observeJobOffersChanges() : window.addEventListener('load', observeJobOffersChanges());
+})();
